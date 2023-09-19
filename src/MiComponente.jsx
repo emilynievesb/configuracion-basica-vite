11226f4ce74bce8ee5ff94cf051165c400f00831
Emily <nemily1710@gmail.com>
2023-09-19 16:02:41 -0500

    feat: :sparkles: Componente MiComponente
    Se realiza componente ejemplo

---
+++ src/MiComponente.jsx
@@ -0,0 +1,10 @@
+function MiComponente() {
+  return (
+    <div>
+      <h1>Mi Componente React</h1>
+      <p>Este es un componente React básico.</p>
+    </div>
+  );
+}
+
+export default MiComponente;
