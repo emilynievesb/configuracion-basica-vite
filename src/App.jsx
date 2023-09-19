d7ea8641bbf318908fd76e49398eb096bd78e6e5
Emily <nemily1710@gmail.com>
2023-09-19 16:02:05 -0500

    feat: :sparkles: App component
    Se realiza componente app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MiComponente from "./MiComponente";
+
+function App() {
+  return (
+    <div className="App">
+      <MiComponente />
+    </div>
+  );
+}
+
+export default App;
